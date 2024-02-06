0c681336bc203c83dd4ac458e5169edcabe7911f
Mahmoud-Azmy <mahmoud.azmyali1902@gmail.com>
2024-02-06 21:19:30 +0200

    implment FetchFeaturedBooksCubit

--- lib/Features/home/presentation/manager/featured_books_cubit/fetch_featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/fetch_featured_books_cubit.dart
@@ -1,9 +1,24 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_featured_books_state.dart';
 
 class FetchFeaturedBooksCubit extends Cubit<FetchFeaturedBooksState> {
-  FetchFeaturedBooksCubit() : super(FetchFeaturedBooksInitial());
+  FetchFeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FetchFeaturedBooksInitial());
+
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FetchFeaturedBooksLoading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold((failuer) {
+      emit(FetchFeaturedBooksFailure(failuer.errorMessage));
+    }, (books) {
+      emit(FetchFeaturedBooksSuccess(books));
+    });
+  }
 }
